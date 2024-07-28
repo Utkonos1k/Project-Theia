6e4cf0bdfc8acb1b823ecc1f6a25c8a2f17290ee
Utkonos <sasabut96@gmail.com>
2024-07-28 22:45:03 +0300

    Day one

--- src/main/java/dev/piteam/projind/item/ModItems.java
+++ src/main/java/dev/piteam/projind/item/ModItems.java
@@ -1,14 +1,16 @@
 package dev.piteam.projind.item;
 
 import dev.piteam.projind.PI;
-import net.neoforged.bus.api.IEventBus;
+import dev.piteam.projind.block.ModBlock;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
-    public static final DeferredRegister.Items ITEMS =
-            DeferredRegister.createItems(PI.MOD_ID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(PI.MOD_ID);
+    public static final DeferredItem<Item> SUPER_GUANNO = ITEMS.register("super_guanno", () -> new Item(new Item.Properties()
+            .stacksTo(2048)
+            .food()));
 
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
 }
