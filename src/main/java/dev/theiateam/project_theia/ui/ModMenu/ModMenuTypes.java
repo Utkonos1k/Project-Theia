cb105b8757d6477f6314950bcbd31d381f5cbe51
Bodbor <bodbor2476@gmail.com>
2025-03-03 16:57:17 +0200

    0.0.3.12 Crusher fix 2

--- src/main/java/dev/theiateam/project_theia/ui/ModMenu/ModMenuTypes.java
+++ src/main/java/dev/theiateam/project_theia/ui/ModMenu/ModMenuTypes.java
@@ -3,6 +3,7 @@ package dev.theiateam.project_theia.ui.ModMenu;
 import dev.theiateam.project_theia.main.ProjectTheia;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.MenuType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -14,5 +15,5 @@ public class ModMenuTypes {
 
     public static final Supplier<MenuType<PrimitiveCrusherMenu>> PRIMITIVE_CRUSHER_MENU = MENU_TYPE.register(
             "primitive_crusher_menu",
-            () -> new MenuType<>(PrimitiveCrusherMenu::new, FeatureFlags.DEFAULT_FLAGS));
+            () -> new MenuType<>((containerId, playerInv) -> new PrimitiveCrusherMenu(containerId, playerInv, ContainerLevelAccess.NULL), FeatureFlags.DEFAULT_FLAGS));
 }
--- src/main/java/dev/theiateam/project_theia/ui/ModMenu/PrimitiveCrusherMenu.java
+++ src/main/java/dev/theiateam/project_theia/ui/ModMenu/PrimitiveCrusherMenu.java
@@ -3,15 +3,18 @@ package dev.theiateam.project_theia.ui.ModMenu;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.item.ItemStack;
 
+import static dev.theiateam.project_theia.block.ModBlock.PRIMITIVE_CRUSHER_WORKBENCH;
 import static dev.theiateam.project_theia.ui.ModMenu.ModMenuTypes.PRIMITIVE_CRUSHER_MENU;
 
 public class PrimitiveCrusherMenu extends AbstractContainerMenu {
-    public PrimitiveCrusherMenu(int containerId, Inventory playerInv) {
+    public PrimitiveCrusherMenu(int containerId, Inventory playerInventory, ContainerLevelAccess aNull) {
         super(PRIMITIVE_CRUSHER_MENU.get(), containerId);
     }
+
+
 
     @Override
     public ItemStack quickMoveStack(Player player, int index) {
@@ -20,6 +23,6 @@ public class PrimitiveCrusherMenu extends AbstractContainerMenu {
 
     @Override
     public boolean stillValid(Player player) {
-        return false;
+        return AbstractContainerMenu.stillValid(this.access, player, PRIMITIVE_CRUSHER_WORKBENCH.get());
     }
 }
